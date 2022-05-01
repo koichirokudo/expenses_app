ec4bcba4ea55ce15466550c0fcad938fe089668c
koichirokudo <koichirokudo746@gmail.com>
2022-05-01 18:14:28 +0900

    Add register page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
-import {Test} from './pages/test';
+import PersonList, {Test} from './pages/test';
 import {Login} from './pages/login';
 import {Register} from './pages/register';
 import {Initial} from './pages/initial';
