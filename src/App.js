c7ebf091c95075088f62c4a988da0dff087988a2
koichirokudo <koichirokudo746@gmail.com>
2022-07-25 12:05:03 +0900

    Move RouterConfig.jsx
    * src/App.js
    * src/config/RouterConfig.jsx

--- src/App.js
+++ src/App.js
@@ -1,30 +1,9 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import { LoginPage } from "./pages/LoginPage";
-import { RegisterPage } from "./pages/RegisterPage";
-import { InputPage } from "./pages/InputPage";
-import { MonthReportPage } from "./pages/MonthReportPage";
-import { CategoryReportPage } from "./pages/CategoryReportPage";
-import { NotFoundPage } from "./pages/NotFoundPage";
+import {RouterConfig} from './config/RouterConfig';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<InputPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/month_report" element={<MonthReportPage />} />
-        <Route
-          path="/category_report"
-          element={<CategoryReportPage />}
-          component={CategoryReportPage}
-          render={() => <CategoryReportPage category={"kudo"} />}
-        />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </BrowserRouter>
+    <RouterConfig/>
   );
 }
 
---
+++ src/config/RouterConfig.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {LoginPage} from "../pages/LoginPage";
+import {RegisterPage} from "../pages/RegisterPage";
+import {InputPage} from "../pages/InputPage";
+import {MonthReportPage} from "../pages/MonthReportPage";
+import {CategoryReportPage} from "../pages/CategoryReportPage";
+import {NotFoundPage} from "../pages/NotFoundPage";
+
+export const RouterConfig = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<InputPage/>}/>
+          <Route path="/login" element={<LoginPage/>}/>
+          <Route path="/register" element={<RegisterPage/>}/>
+          <Route path="/month_report" element={<MonthReportPage/>}/>
+          <Route
+            path="/category_report"
+            element={<CategoryReportPage/>}
+            component={CategoryReportPage}
+            render={() => <CategoryReportPage category={"kudo"}/>}
+          />
+          <Route path="*" element={<NotFoundPage/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
